4118d8b37e6b5ca0922b586777718260d48bd636
kenshi <samuduchanuka@gmail.com>
2024-01-27 22:42:12 +0530

    Add functionality to Clear and Delete buttons, to display values, get the sum of two values.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,39 @@
-let num1;
-let num2;
-let operator;
+const btn_values = document.querySelectorAll(".btn");
+const btn_equals = document.querySelector(".btn_equals");
+const screen_input = document.querySelector(".screen_input");
+const screen_output = document.querySelector(".screen_output");
+const btn_clear = document.querySelector(".btn_clear");
+const btn_delete = document.querySelector(".btn_delete");
 
+let display_value = '';
 
+btn_values.forEach((key) => {
+    key.addEventListener("click", (event) => {
+        display_value += event.target.textContent;
+        screen_input.textContent = display_value;
+    })
+})
+
+btn_clear.addEventListener("click", () => {
+    display_value = '';
+    screen_input.textContent = display_value;
+    screen_output.textContent = display_value;
+})
+
+btn_delete.addEventListener("click", () => {
+    display_value = display_value.slice(0, -1);
+    screen_input.textContent = display_value;
+})
+
+btn_equals.addEventListener("click", (event) => {
+    let values = display_value.split('+');
+    let num1 = parseFloat(values[0]);
+    let num2 = parseFloat(values[1]);
+    let operator = '+';
+    let result = operate(num1, operator, num2);
+
+    screen_output.textContent = result;
+})
 
 function add(num1, num2){
     return num1 + num2;
@@ -19,7 +50,6 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1 / num2;
 }
-
 
 function operate(num1, operator, num2){
     switch (operator){

74deeb0aa5c109d2845f5d3dfe95805d81f52fde
kenshi <samuduchanuka@gmail.com>
2024-01-27 19:53:45 +0530

    Initial commit. Designed a simple interface.

---
+++ scripts.js
@@ -0,0 +1,38 @@
+let num1;
+let num2;
+let operator;
+
+
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function substract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+
+function operate(num1, operator, num2){
+    switch (operator){
+        case '+': 
+            return add(num1, num2);
+
+        case '-': 
+            return substract(num1, num2);
+
+        case '*': 
+            return multiply(num1, num2);
+
+        case '/': 
+            return divide(num1, num2);
+    }
+}
